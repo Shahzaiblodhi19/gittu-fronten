aac0f5011bb934a030441781eb7d8850ec00f8c6
goldentoken6 <sonhonestdev@gmail.com>
2025-03-15 05:01:14 +0900

    updated contract address

--- src/pages/web3/constants.js
+++ src/pages/web3/constants.js
@@ -16,8 +16,8 @@ export const IS_MAINNET = DEFAULT_NETWORK == Networks.MAINNET;
 
 export const PROGRAM_ID = new PublicKey(
   IS_MAINNET ?
-    "FE4VF2CMGgtrzpCaTUuMwumN2H7cBXwic6LmjRCUyHc5" :
-    "F1ufkRJ2ZR9ChJms4fjiKqVK1Hh699X43TfYeQMzpfHT"
+    "5KzCBkpXMSjtFtjyQZSSBzJ9H1yWKFikV3Ah3kSUX7t1" :
+    "5KzCBkpXMSjtFtjyQZSSBzJ9H1yWKFikV3Ah3kSUX7t1"
 );
 
 export const SEC = new PublicKey(
@@ -28,16 +28,14 @@ export const SEC = new PublicKey(
 
 export const aSEC = new PublicKey("DDREXsQb24eX5hJQKm8L5tyGxRX3XUkfn7gKvTfWziv1")
 
-export const PRICE_PER_TOKEN = 0.00005; // sol
-
-export const SEC_DECIMALS = IS_MAINNET ? 6 : 9;
+export const SEC_DECIMALS = IS_MAINNET ? 6 : 6;
 export const SOL_DECIMALS = 9;
 
 export const PRESALE_START_TIMESTAMP = 1717750800; // Thu Jun 06 2024 00:00:00 GMT+0000 //1717750800
 export const PRESALE_END_TIMESTAMP = 1781725200; // 	Sun Jul 21 2024 00:00:00 GMT+0000    presale takes 45 days.
 
-// export const TOKEN_PUBKEY = new PublicKey("AY2pTeKZfwazVPxcyky3BenUPhpLpprbXUyN4uhVoz1P");
-// export const ALT_TOKEN_PUBKEY = new PublicKey("7ZUEDUKzjRf6ZEJtWUq9Q5svXSaFf2LxdWrSCY39idre");
+export const TOKEN_PUBKEY = new PublicKey("AY2pTeKZfwazVPxcyky3BenUPhpLpprbXUyN4uhVoz1P");
+export const ALT_TOKEN_PUBKEY = new PublicKey("7ZUEDUKzjRf6ZEJtWUq9Q5svXSaFf2LxdWrSCY39idre");
 
-// export const TOKEN_PRESALE_HARDCAP = 4000000000; // token
-// export const PRICE_PER_TOKEN = 0.0000002; // sol
+export const TOKEN_PRESALE_HARDCAP = 4000000000; // token
+export const PRICE_PER_TOKEN = 0.0000002; // sol
