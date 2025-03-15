83685a59a123a116cbaefa4c85ca6801eb49034c
Shahzaibllodhi19 <shahzaiblodhi2233@gmail.com>
2025-03-15 14:38:32 +0500

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -396,6 +396,21 @@ function Home() {
         setIsModalOpen(!isModalOpen);
     };
 
+
+    useEffect(() => {
+        if (isModalOpen) {
+            // Disable scrolling when the modal is open
+            document.body.style.overflow = 'hidden';
+        } else {
+            // Re-enable scrolling when the modal is closed
+            document.body.style.overflow = 'auto';
+        }
+
+        // Cleanup: reset overflow when the component unmounts
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [isModalOpen]);
     return (
         <>
             <div className="app-section" id="main">
@@ -546,7 +561,7 @@ function Home() {
                     >
                         <button className="w-auto close mt-0 ml-auto" onClick={() => setIsModalOpen(false)} style={{ padding: '11px', borderRadius: '50px', marginLeft: 'auto' }}><svg aria-hidden="true" fill="none" height="10" viewBox="0 0 10 10" width="10" xmlns="http://www.w3.org/2000/svg"><title>Close</title><path d="M1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L3.58579 5L0.292893 8.29289C-0.0976311 8.68342 -0.0976311 9.31658 0.292893 9.70711C0.683417 10.0976 1.31658 10.0976 1.70711 9.70711L5 6.41421L8.29289 9.70711C8.68342 10.0976 9.31658 10.0976 9.70711 9.70711C10.0976 9.31658 10.0976 8.68342 9.70711 8.29289L6.41421 5L9.70711 1.70711C10.0976 1.31658 10.0976 0.683417 9.70711 0.292893C9.31658 -0.0976311 8.68342 -0.0976311 8.29289 0.292893L5 3.58579L1.70711 0.292893Z" fill="currentColor"></path></svg></button>
                         <div className="content text-center">
-                            <h2 style={{fontSize: '18px'}}>Successfully Purchased</h2>
+                            <h2 style={{ fontSize: '18px' }}>Successfully Purchased</h2>
                         </div>
 
                     </ReactModal>
